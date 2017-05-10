a34211d1f87fdf24068763c8df4cd6b32aacc908
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-05-10 11:06:10 +0200

    Initial Commit.

---
+++ AadMigration.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AadMigration.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ AadMigration.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AadMigration.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
