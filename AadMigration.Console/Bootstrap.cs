f7d07c1ce99577314cbf0b6f6b6530534c7ecf2b
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-05-10 17:49:02 +0200

    Add DI with dryioc. Add settings. Refacto.

---
+++ AadMigration.Console/Bootstrap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AadMigration.Common.GraphApi;
+using AadMigration.Common.LoginApi;
+using AadMigration.Common.Settings;
+using DryIoc;
+using DryIoc.Microsoft.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AadMigration.Console
+{
+    public static class Bootstrap
+    {
+        public static IServiceProvider ConfigureServices(IServiceCollection services, IConfigurationRoot configuration)
+        {
+            services.AddLogging();
+
+            services.Configure<TenantSettings>(configuration.GetSection("TenantSettings"));
+
+            var container = new Container().WithDependencyInjectionAdapter(services);
+
+            container.Register<IGraphApiService, GraphApiService>();
+            container.Register<ITokenService, TokenService>();
+
+            return container.Resolve<IServiceProvider>();
+        }
+    }
+}
--- AadMigration.Console/Program.cs
+++ AadMigration.Console/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace AadMigration.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            IServiceProvider provider = Bootstrap.ConfigureServices(new ServiceCollection(), configuration);
+            
+            provider
+                .GetService<ILoggerFactory>()
+                .AddConsole(configuration.GetSection("Logging"));
         }
     }
 }
