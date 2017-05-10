9a06077c74b3f4ef76f2382c4416068f87be524c
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-05-10 14:44:33 +0200

    Add graph api contracts.

---
+++ AadMigration.Common/GraphApi/IGraphApi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Refit;
+
+namespace AadMigration.Common.GraphApi
+{
+    public interface IGraphApi
+    {
+        [Get("/users/{userId}?api-version={apiVersion}")]
+        Task<HttpResponseMessage> FindUserAsync(string userId, [Header("Authorization")] string authorization, string apiVersion = "1.6");
+
+        [Get("/users?api-version={apiVersion}")]
+        Task<IEnumerable<User>> GetUsersAsync([Header("Authorization")] string authorization, string apiVersion = "1.6");
+
+        [Post("/users?api-version={apiVersion}")]
+        Task PostUserAsync([Body] User user, [Header("Authorization")] string authorization, string apiVersion = "1.6");
+    }
+}
---
+++ AadMigration.Common/GraphApi/User.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace AadMigration.Common.GraphApi
+{
+    public class User
+    {
+        public bool AccountEnabled { get; set; }
+        public IEnumerable<Signinname> SignInNames { get; set; }
+        public string CreationType { get; set; }
+        public string DisplayName { get; set; }
+        public string MailNickname { get; set; }
+        public Passwordprofile PasswordProfile { get; set; }
+        public string UserPrincipalName { get; set; }
+    }
+
+    public class Passwordprofile
+    {
+        public bool ForceChangePasswordNextLogin { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Signinname
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
